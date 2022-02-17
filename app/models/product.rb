b445441e69ed771bc55ac36e5ff804be047ec770
lemanhcuongSun <le.manh.cuong-b@sun-asterisk.com>
2022-02-17 09:51:26 +0700

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,22 +11,28 @@ class Product < ApplicationRecord
     joins(:product_details)
       .where(product_details: {product_size_id: product_size_id})
   end)
+
   scope :filter_by_product_color_id, (lambda do |product_color_id|
     joins(:product_details)
       .where(product_details: {product_color_id: product_color_id})
   end)
+
   scope :filter_by_max_cost, (lambda do |max_cost|
     joins(:product_details)
       .group("product_id")
       .having("avg(product_details.cost) < ?", max_cost)
   end)
+
   scope :filter_by_min_cost, (lambda do |min_cost|
     joins(:product_details)
       .group("products.id")
       .having("avg(product_details.cost) > ?", min_cost)
   end)
+
   scope :search, ->(key){where("LOWER(name) like ?", "%#{key.downcase}%")}
+
   scope :order_created_at, ->{order(created_at: :desc)}
+
   scope :top_sellers, (lambda do |size|
     joins(product_details: :order_details)
       .select("products.*, SUM(order_details.quantity) as sum_order")
